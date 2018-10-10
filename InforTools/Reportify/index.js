51a9aa71fdcf3723cfaea0da0f807898272684d2
brykabry <bryan.k.c.san.jose@live.com.ph>
2018-10-11 00:51:50 +0800

    yo

--- InforTools/Reportify/index.js
+++ InforTools/Reportify/index.js
@@ -36,23 +36,14 @@ $(document).ready(function(){
                 })
             },
             download:function(){
-                $.cors({
-                    'url': "http://usspw565.lawson.com/lars/BuildRepositoryForWeb/list/Build(HCM,34,_niu_).ActiveListForPanel?csk.3x=true&dependentList=true&pageSize=20&pageop=load&relation=BuildStream(HCM%2C34).Build_ByBuildStream_UsingSymbolicKey_SetRel&_=" + Date.now(),
-                    'success': function (data) {
-                        // Code here!
-                    },
-                    'error': function () {
-                        // Code here!
-                    }
-                });
+                console.log("checkpoint! research about cors!")
 
 
 
 
 
 
-
-
+    
 
 
 
