daaf83c550c36a4bddf63ca4a7de67eb1109864b
brykabry <bryan.k.c.san.jose@live.com.ph>
2018-10-10 23:27:18 +0800

    Update InforTools/Reportify/index.js

--- InforTools/Reportify/index.js
+++ InforTools/Reportify/index.js
@@ -2,9 +2,17 @@ $(document).ready(function(){
     var brykabry = {
         events:{
             init:function(){
-                $(".title-wrapper.reportify").css({"transform":"scale(1.5)"})
-                setTimeout(function(){$(".title-wrapper.reportify").css({"transform":"scale(1)"})},200);
-                $(".title-wrapper.reportify").on("mouseenter",function(){
+                brykabry.events.animateBorder();
+                $(".title-wrapper").on("click",function(){
+                    $(".title-wrapper.reportify").css({"transform":"scale(1.5)"}).on("mouseleave",function(){$(".borderbottom, .bordertop").css({"margin-left":"0"});$(".borderright, .borderleft").css({"margin-top":"0"});});
+                    setTimeout(function(){$(".title-wrapper.reportify").css({"transform":"scale(0)"})},200);
+                    brykabry.events.showDownload();
+                })
+            },
+            animateBorder:function(){
+                $(".title-wrapper").css({"transform":"scale(1.5)"})
+                setTimeout(function(){$(".title-wrapper").css({"transform":"scale(1)"})},200);
+                $(".title-wrapper").on("mouseenter",function(){
                     $(".borderbottom, .bordertop").css({"margin-left":"0"});
                     $(".borderright, .borderleft").css({"margin-top":"0"});
                 }).on("mouseleave",function(){
@@ -12,10 +20,6 @@ $(document).ready(function(){
                     $(".borderright").css({"margin-top":"105px"});
                     $(".borderleft").css({"margin-top":"-105px"});
                     $(".bordertop").css({"margin-left":"550px"});    
-                }).on("click",function(){
-                    $(".title-wrapper.reportify").css({"transform":"scale(1.5)"}).on("mouseleave",function(){$(".borderbottom, .bordertop").css({"margin-left":"0"});$(".borderright, .borderleft").css({"margin-top":"0"});});
-                    setTimeout(function(){$(".title-wrapper.reportify").css({"transform":"scale(0)"})},200);
-                    brykabry.events.showDownload();
                 })
             },
             showDownload:function(){
