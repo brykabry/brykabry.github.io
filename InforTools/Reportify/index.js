f770be1c391afddd6ff3c964c9bb03dcb3f5d00d
brykabry <bryan.k.c.san.jose@live.com.ph>
2018-10-11 01:06:12 +0800

    test
    e

--- InforTools/Reportify/index.js
+++ InforTools/Reportify/index.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
             download:function(){
                 console.log("checkpoint! research about cors!")
                 $.ajax({
-                    url: "http://wiki.lawson.com/display/HCM/Human+Capital+Management+Servers",
+                    url:  "http://usspw565.lawson.com/lars/BuildRepositoryForWeb/list/Build(HCM,34,_niu_).ActiveListForPanel?csk.3x=true&dependentList=true&pageSize=20&pageop=load&relation=BuildStream(HCM%2C34).Build_ByBuildStream_UsingSymbolicKey_SetRel&_=" + Date.now(),
                     headers: {  'Access-Control-Allow-Origin': 'https://brykabry.github.io' },
                     dataType: 'json',
                     /* etc */
