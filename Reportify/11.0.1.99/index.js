3a2de4b4f84ff7bc7ec6f98891d60a3342a0d2f9
brykabry <bryan.k.c.san.jose@live.com.ph>
2018-10-10 21:53:57 +0800

    Add files via upload

--- Reportify/11.0.1.99/index.js
+++ Reportify/11.0.1.99/index.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
     var brykabry = {
         events:{
            init:function(){
-                $(".title-wrapper").on("mouseenter",function(){
-                    setTimeout(function(){$(".borderbottom").css({"width":"550px"})},100);
-                    setTimeout(function(){$(".borderleft").css({"height":"100px"})},200);
-                    setTimeout(function(){$(".bordertop").css({"width":"550px"})},300);
-                    setTimeout(function(){$(".borderright").css({"height":"100px"})},400);
+                // $(".title-wrapper").on("mouseenter",function(){
+                //     setTimeout(function(){$(".borderbottom").css({"width":"550px"})},100);
+                //     setTimeout(function(){$(".borderleft").css({"height":"100px"})},200);
+                //     setTimeout(function(){$(".bordertop").css({"width":"550px"})},300);
+                //     setTimeout(function(){$(".borderright").css({"height":"100px"})},400);
                     
-                }).on("mouseleave",function(){
-                    setTimeout(function(){$(".borderright").css({"height":"0"})},100);
-                    setTimeout(function(){$(".bordertop").css({"width":"0"})},200);
-                    setTimeout(function(){$(".borderleft").css({"height":"0"})},300);
-                    setTimeout(function(){$(".borderbottom").css({"width":"0"})},400);
-                })
+                // }).on("mouseleave",function(){
+                //     setTimeout(function(){$(".borderright").css({"height":"0"})},100);
+                //     setTimeout(function(){$(".bordertop").css({"width":"0"})},200);
+                //     setTimeout(function(){$(".borderleft").css({"height":"0"})},300);
+                //     setTimeout(function(){$(".borderbottom").css({"width":"0"})},400);
+                // })
            
            }
             
