49fef24f0bef32105f69204d7f5562223c1938d7
brykabry <bryan.k.c.san.jose@live.com.ph>
2018-10-10 21:34:39 +0800

    Add files via upload

--- Reportify/11.0.1.99/index.js
+++ Reportify/11.0.1.99/index.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-    
+    var brykabry = {
+        events:{
+           init:function(){
+                $(".title-wrapper").on("mouseenter",function(){
+                    setTimeout(function(){$(".borderbottom").css({"width":"550px"})},200);
+                    setTimeout(function(){$(".borderleft").css({"height":"100px"})},400);
+                    setTimeout(function(){$(".bordertop").css({"width":"550px"})},600);
+                    setTimeout(function(){$(".borderright").css({"height":"100px"})},800);
+                    
+                }).on("mouseleave",function(){
+                    setTimeout(function(){$(".borderright").css({"height":"0"})},800);
+                    setTimeout(function(){$(".bordertop").css({"height":"0"})},600);
+                    setTimeout(function(){$(".borderleft").css({"height":"0"})},400);
+                    setTimeout(function(){$(".borderbottom").css({"width":"0"})},200);
+                })
+           
+           }
+            
+        }
+    }
+    brykabry.events.init();
 })
