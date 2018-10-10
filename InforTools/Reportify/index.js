06005db7b858502791eba10365956d31228c9a69
brykabry <bryan.k.c.san.jose@live.com.ph>
2018-10-10 23:54:50 +0800

    Update InforTools/Reportify/index.js

--- InforTools/Reportify/index.js
+++ InforTools/Reportify/index.js
@@ -31,8 +31,80 @@ $(document).ready(function(){
             showDownload:function(){
                 brykabry.events.animateBorder(".download");
                 setTimeout(function(){$(".title-wrapper.download").css({"transform":"scale(1)"})},200);
+                $(".download.eleven-o-one").on("click",function(){
+                    brykabry.events.download("eleven-o-one");
+                })
             },
             download:function(){
+                var global = {
+                    branch:"",
+                    xhr : "",
+                    latestBuild: "",
+                    issueList: "",
+                    issueCounter: 0,
+                    normalize: [],
+                    currentServerBuild: ""
+                }
+                alert("Generating a report might take a while. Press OK to continue.");
+                var url = "http://usspw565.lawson.com/lars/BuildRepositoryForWeb/list/Build(HCM,34,_niu_).ActiveListForPanel?csk.3x=true&dependentList=true&pageSize=20&pageop=load&relation=BuildStream(HCM%2C34).Build_ByBuildStream_UsingSymbolicKey_SetRel&_=" + Date.now();
+                var server= "http://wiki.lawson.com/display/HCM/Human+Capital+Management+Servers"
+                ajax("getCurrentServerBuild",server);
+                ajax("init",url);
+
+                var ajax = function(){
+                    var xhr = new XMLHttpRequest();
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState == XMLHttpRequest.DONE) {
+                        switch(key){
+                            case "init":
+                            global.xhr = xhr.responseText
+                            getLatestBuild();
+                            break;
+                            case "getIssueLists":
+                            global.issueList = JSON.parse(xhr.responseText).dataViewSet.data;
+                            global.issueCounter = global.issueList.length-1;
+                            getResolutionTickets();
+                            break;
+                            case "getResolutionTickets":
+                            getResolutionDetails(JSON.parse(xhr.responseText),url.split("http://jira.lawson.com/rest/api/2/issue/")[1])
+                            break;
+                            case "getResolutionDetails":
+                            if(global.issueCounter!=0){
+                                addResolutionDetails(JSON.parse(xhr.responseText),url.split("http://jira.lawson.com/rest/api/2/issue/")[1]);
+                                global.issueCounter--;                    
+                            }else{
+                                addResolutionDetails(JSON.parse(xhr.responseText),url.split("http://jira.lawson.com/rest/api/2/issue/")[1]);
+                                normalizeDataForDownload();
+                            }
+                            break;
+                            case "getCurrentServerBuild":
+                            global.currentServerBuild = xhr.responseText.toString().split("11.0.1.99.")[1].split("</td>")[0];
+                            break;
+                        }
+                        
+                    }
+                    xhr.open('GET', url, true);
+                    xhr.send(null);
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             }
             
