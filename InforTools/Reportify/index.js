2f677cb76d578bc639616f553e9f35874c449734
brykabry <bryan.k.c.san.jose@live.com.ph>
2018-10-16 21:49:04 +0800

    Update InforTools/Reportify/index.js

--- InforTools/Reportify/index.js
+++ InforTools/Reportify/index.js
@@ -38,9 +38,44 @@ $(document).ready(function(){
             download:function(){
                 console.log("checkpoint! research about cors!");
                 firebase.database().ref('report/110001').once('value').then(function(snapshot) {
-                    console.log(snapshot.val().data)
-                  });
-            }
+                    JSONToCSVConvertor(snapshot.val().data,"", true)
+                });
+            },
+            JSONToCSVConvertor:function (JSONData, ReportTitle, ShowLabel) {
+                var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+                var CSV = '';
+                if (ShowLabel) {
+                    var row = "";
+                    for (var index in arrData[0]) {
+                        row += index + ',';
+                    }
+                    row = row.slice(0, -1);
+                    CSV += row + '\r\n';
+                }
+                for (var i = 0; i < arrData.length; i++) {
+                    var row = "";
+                    for (var index in arrData[i]) {
+                        row += '"' + arrData[i][index] + '",';
+                    }
+                    row.slice(0, row.length - 1);
+                    CSV += row + '\r\n';
+                }
+                if (CSV == '') {        
+                    alert("Invalid data");
+                    return;
+                }   
+                var datenow = new Date()
+                var fileName = "GHR/TM CU Report as of " + datenow.toLocaleDateString();
+                fileName += ReportTitle.replace(/ /g,"_");   
+                var uri = 'data:text/csv;charset=utf-8,' + CSV;
+                var link = document.createElement("a");    
+                link.href = uri;
+                link.style = "visibility:hidden";
+                link.download = fileName + ".csv";
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+              }
             
         }
     }
