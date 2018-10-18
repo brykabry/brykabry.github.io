0957d989dc1274611b722e0af477a6d7af010afb
brykabry <bryan.k.c.san.jose@live.com.ph>
2018-10-19 03:23:17 +0800

    yeah

--- InforTools/TouchBase/js/index.js
+++ InforTools/TouchBase/js/index.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
+    $.fn.extend({
+        qcss: function(css) {
+           return $(this).queue(function(next) {
+              $(this).css(css);
+              next();
+           });
+        }
+     });
     var src = $('.bg').css('background-image');
     var url = src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
     
     var img = new Image();
     img.onload = function() {
-        tb.events.showTitle();
+        setTimeout(function(){
+            tb.events.showTitle();
+        },750)
+        
     }
     img.src = url;
     if (img.complete) img.onload();
@@ -13,6 +24,18 @@ $(document).ready(function(){
     var tb = {
         events:{
             showTitle:function(){
+                var timeout = 375;
+                var blinker = $(".label.blinker");
+                blinker
+                .delay(timeout).qcss({"visibility":"visible"})
+                .delay(timeout).qcss({"visibility":"hidden"})
+                .delay(timeout).qcss({"visibility":"visible"})
+                .delay(timeout).qcss({"visibility":"hidden","height":"0"})
+                // .delay(timeout).qcss({"visibility":"visible"})
+                .delay(timeout).qcss({"visibility":"visible","height":"60px"})
+                var touchbase = $(".label.touchbase .wrapper");
+                touchbase.delay((timeout*5)+timeout).qcss({"margin-left":"0"})
+
                 
             }
         }
