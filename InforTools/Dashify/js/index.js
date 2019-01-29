4b8c4c5d4409a0c1c4ceea217190d7ca8cd526a8
brykabry <bryan.k.c.san.jose@live.com.ph>
2019-01-29 23:38:14 +0800

    test
    test

---
+++ InforTools/Dashify/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    var brykabry = {
+        events:{
+           init:function(){
+                console.log("test")
+           }
+            
+        }
+    }
+    var src = $('.bg').css('background-image');
+    var url = src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
+
+    var img = new Image();
+    img.onload = function() {
+        setTimeout(function(){brykabry.events.init()},500);
+    }
+    img.src = url;
+    if (img.complete) img.onload();
+    
+})
--- TradeTools/CandleScreener/runInConsole.js
+++ TradeTools/CandleScreener/runInConsole.js
@@ -6,6 +6,7 @@ setTimeout(() => {
     currentStock = ""
     threeInsideUpArray = [];
     donchianSwingArray = [];
+    aroonBearSwingArray = [];
     SAjax = function(url,success){
         $.ajax({
             url: url,
@@ -27,6 +28,41 @@ setTimeout(() => {
         if(PO >= PC && CL >= PL && CC >= CO){
             return true
         }else{ return false}
+
+    }
+    aroonBearSwing = function(ze,on,tw,th,fo,fi,si,se,ei,ni,te){
+        data = [ze.sqLow,on.sqLow,tw.sqLow,th.sqLow,fo.sqLow,fi.sqLow,si.sqLow,se.sqLow,ei.sqLow,ni.sqLow,te.sqLow];
+        //data = [0,1,2,3,4,5,6,7,8,9,10]
+        aroonBearLowOne = data[10]
+        aroonBearLowIndicatorOne = 100
+        for(var i = 9, k = 0; i>k;i--){
+            if(data[i]<aroonBearLowOne){
+                aroonBearLowOne=data[i];
+                aroonBearLowIndicatorOne=100
+
+            }else{
+                aroonBearLowIndicatorOne-=10;
+            }
+        }
+        //data = [0,1,2,3,4,5,6,7,8,9,10]
+        aroonBearLowTwo = data[9]
+        aroonBearLowIndicatorTwo = 100
+        for(var i = 8, k = 0; i>=k;i--){
+            if(data[i]<aroonBearLowTwo){
+                aroonBearLowTwo=data[i];
+                aroonBearLowIndicatorTwo=100
+
+            }else{
+                aroonBearLowIndicatorTwo-=10;
+            }
+        }
+        if(aroonBearLowIndicatorOne == 100 && aroonBearLowIndicatorTwo < 100){
+            
+            return true
+        }else{
+            return false
+        }
+
 
     }
     for(var i = 0, k = stockList.length; k > i; i++){
@@ -71,6 +107,19 @@ setTimeout(() => {
                     if (donchianSwing(CH,CO,CC,CL,PH,PO,PC,PL,PPH,PPO,PPC,PPL)&&aveVal()>=1000000){//
                         donchianSwingArray.push(currentStock);
                     }
+                    if (aroonBearSwing(JSON.parse(e).records[0],
+                        JSON.parse(e).records[1],
+                        JSON.parse(e).records[2],
+                        JSON.parse(e).records[3],
+                        JSON.parse(e).records[4],
+                        JSON.parse(e).records[5],
+                        JSON.parse(e).records[6],
+                        JSON.parse(e).records[7],
+                        JSON.parse(e).records[8],
+                        JSON.parse(e).records[9],
+                        JSON.parse(e).records[10])&&aveVal()>=1000000){//
+                            aroonBearSwingArray.push(currentStock);
+                    }
                 } catch (error) {
                     console.log(error)
                 }
@@ -79,7 +128,9 @@ setTimeout(() => {
     }
     console.log(threeInsideUpArray);
     console.log(donchianSwingArray);
+    console.log(aroonBearSwingArray);
     $("body").html(`<div class='threeInsideUp'>ThreeInsideUp: ${threeInsideUpArray.join("\n")}</div>
-    <div class='haramiOrTweezer'>DonchianSwing: ${donchianSwingArray.join("\n")}</div>
+    <div class='donchianSwing'>DonchianSwing: ${donchianSwingArray.join("\n")}</div>
+    <div class='aroonSwing'>DonchianSwing: ${aroonBearSwingArray.join("\n")}</div>
     `)
 }, 1000);
