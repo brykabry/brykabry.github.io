dc7412374f836c860c608b23091e8f961833273e
brykabry <bryan.k.c.san.jose@live.com.ph>
2019-08-23 17:54:29 +0800

    Update index.js

--- InforTools/Reportify/index.js
+++ InforTools/Reportify/index.js
@@ -75,7 +75,11 @@ $(document).ready(function(){
                 for (var i = 0; i < arrData.length; i++) {
                     var row = "";
                     for (var index in arrData[i]) {
-                        row += '"' + arrData[i][index] + '",';//.replace(/"/g,"'")
+                        if(arrData[i][index].indexOf("HYPERLINK") > "-1"){
+                            row += '"' + arrData[i][index] + '",';//.replace(/"/g,"'")
+                        }else{
+                            row += '"' + arrData[i][index].replace(/"/g,"'") + '",';
+                        }
                     }
                     row.slice(0, row.length - 1);
                     CSV += row + '\r\n';
