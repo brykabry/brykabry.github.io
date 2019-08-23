9aecc37e00c43cb0d16cca09fe00c3e8697ffd68
brykabry <bryan.k.c.san.jose@live.com.ph>
2019-08-23 14:55:16 +0800

    Update index.js

--- InforTools/Reportify/index.js
+++ InforTools/Reportify/index.js
@@ -75,7 +75,7 @@ $(document).ready(function(){
                 for (var i = 0; i < arrData.length; i++) {
                     var row = "";
                     for (var index in arrData[i]) {
-                        row += '"' + arrData[i][index] + '",';
+                        row += '"' + arrData[i][index].replace(/"/g,"'") + '",';
                     }
                     row.slice(0, row.length - 1);
                     CSV += row + '\r\n';
