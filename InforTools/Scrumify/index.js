47d65076cac3da99bd9298e012ffd91d72aaf6ab
brykabry <bryan.k.c.san.jose@live.com.ph>
2021-10-06 00:26:00 +0800

    changes

--- InforTools/Scrumify/index.js
+++ InforTools/Scrumify/index.js
@@ -205,9 +205,9 @@ function markUp(id,title,desc) {
         <div class="card-title">
             <div class="lead font-weight-light lightBlue id">${title}</div>
         </div>
-        <div class="description">
+        <p class="description">
             ${desc}
-        </div>
+        </p>
         <hr>
         <button class="btn btn-danger btn-sm btn-block deletebtn" onclick="deleteItem('${id}')">Delete</button>
     </div>
