cf55b44d3969d684469628945af811cf659acb7e
brykabry <bryan.k.c.san.jose@live.com.ph>
2021-10-05 23:26:11 +0800

    Update index.js

--- InforTools/Scrumify/index.js
+++ InforTools/Scrumify/index.js
@@ -95,25 +95,25 @@ firebase.database().ref(scrum).on('value', function(snapshot) {
         if(typeof data.todo != "undefined" ){
             // $(".todo .items .card").remove()
             $(data.todo).each(function(e,v){
-                $(".todo .items").append(markUp(v.id,v.description))
+                $(".todo .items").append(markUp(v.id,v.title,v.description))
             })
         }
         if(typeof data.inprogress != "undefined" ){
             // $(".inprogress .items .card").remove()
             $(data.inprogress).each(function(e,v){
-                $(".inprogress .items").append(markUp(v.id,v.description))
+                $(".inprogress .items").append(markUp(v.id,v.title,v.description))
             })
         }
         if(typeof data.testing != "undefined" ){
             // $(".testing .items .card").remove()
             $(data.testing).each(function(e,v){
-                $(".testing .items").append(markUp(v.id,v.description))
+                $(".testing .items").append(markUp(v.id,v.title,v.description))
             })
         }
         if(typeof data.done != "undefined" ){
             // $(".done .items .card").remove()
             $(data.done).each(function(e,v){
-                $(".done .items").append(markUp(v.id,v.description))
+                $(".done .items").append(markUp(v.id,v.title,v.description))
             })
         }
     }else{
@@ -198,12 +198,12 @@ $('#editModal').on('show.bs.modal', function (event) {
     modal.find('.modal-body input').val(recipient)
   })
 
-function markUp(id,desc) {
+function markUp(id,title,desc) {
     
     return `<div class="card draggable shadow-sm" id="${id}" draggable="true" ondragstart="drag(event)">
     <div class="card-body p-2">
         <div class="card-title">
-            <div class="lead font-weight-light lightBlue id">${id.replace(/BBB/g," ")}</div>
+            <div class="lead font-weight-light lightBlue id">${title}</div>
         </div>
         <div class="description">
             ${desc}
